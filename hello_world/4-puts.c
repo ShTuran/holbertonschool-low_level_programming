bbf29d1470958ac952044c9a563025fd57e78b40
ShTuran <turan.shrinov@gmail.com>
2023-10-15 13:24:03 -0400

    printing string with puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the desired string with a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	putchar('\n');
 	return (0);
 }
+
