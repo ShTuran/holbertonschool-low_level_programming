6fffe477419cfd6cbc1ae62af7fbe5f772535213
ShTuran <turan.shrinov@gmail.com>
2023-10-17 16:23:35 -0400

    prints the digits 0-9

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,27 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the digits 0-9
+ * Description: This program prints the digits 0-9.
  *
  * Return: Always 0 (Success)
  */
-
-
-int main (void)
+int main(void)
 {
-	int digit = 0 ;
+    int digit = 0;
 
-	while (digit<10)
-	{
+    while (digit < 10)
+    {
+        putchar('0' + digit);
+        digit++;
+    }
 
-		putchar('0'+digit);
-		digit++;
+    putchar('\n');
 
-	}
-	
-	putchar('\n');
-
-
-
-	return (0);
+    return (0);
 }
+
