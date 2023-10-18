4479ef5f4b4dd1ef6e87c754ba8a82972fbf5fc5
ShTuran <turan.shrinov@gmail.com>
2023-10-18 10:54:15 -0400

    prints the digits 0-9

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the digits 0-9.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int digit=0;
 
-	while(digit<10)
+	while (digit < 10)
+
 	{
 		putchar(digit);
 		digit++;
 	}
+
 	putchar('\n');
 
 	return (0);
