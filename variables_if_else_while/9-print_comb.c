7a40c4c99e43caabc2e3a704923efd0f75be5a94
ShTuran <turan.shrinov@gmail.com>
2023-10-18 14:15:12 -0400

    prints all possible combinations of single-digit numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: This program prints all possible combinations of single-digit numbers.
+ * Description: Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
