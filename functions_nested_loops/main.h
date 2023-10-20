23e5bd9861de0268c31dc64a62895d386f3e8df5
Turan <111232034+ShTuran@users.noreply.github.com>
2023-10-20 19:03:19 +0400

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+#include "main.h"
 
-int _putchar(char c);
-
-void print_alphabet(void);
-
-#endif /* MAIN_H */
-
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
