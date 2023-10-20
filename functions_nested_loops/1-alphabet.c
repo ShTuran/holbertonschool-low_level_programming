a778f95ac11cc811fbb6731b2b39331090e3a3ff
ShTuran <turan.shrinov@gmail.com>
2023-10-20 10:43:31 -0400

    that prints the alphabet, in lowercase, followed by a new line

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+#define _XOPEN_SOURCE 700
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints  _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void)
+{
+
+
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar("\n");
+
+
+
+
+
+
+
+
+
+	return(0);
+}
