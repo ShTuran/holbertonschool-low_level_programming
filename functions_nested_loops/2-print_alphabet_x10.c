c031bb77ab47504252a9b5f33211a888f24c67d6
ShTuran <turan.shrinov@gmail.com>
2023-10-21 04:07:47 -0400

    10 times alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet ten times, followed by a newline.
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times.
  */
 
 void print_alphabet_x10(void)
