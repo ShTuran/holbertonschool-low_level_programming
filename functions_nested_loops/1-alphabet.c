967e82c189447d55bbb92e6c5d4bbb329e2e62f0
ShTuran <turan.shrinov@gmail.com>
2023-10-21 02:44:20 -0400

    prints alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
-
-*	print_alphabet - Prints the lowercase alphabet followed by a newline.
-
-*/
-
+ * print_alphabet - Prints the lowercase alphabet followed by a newline.
+ */
 
 void print_alphabet(void)
 {
-
 	char letter = 'a';
 
 	while (letter <= 'z')
@@ -21,7 +16,5 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
+
 }
-
-
-
