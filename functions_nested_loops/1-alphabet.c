7c90a267631410c2cf6ec8adbc59a5256a609058
ShTuran <turan.shrinov@gmail.com>
2023-10-21 02:37:02 -0400

    prints alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+
 /**
-*	main - Entry point
-*
-*	Description: Prints  _putchar, followed by a new line.
-*
-*	Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Prints  _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 void print_alphabet(void)
