6e0b9872d1ed1b2183276dc917eff41123a0a6ea
ShTuran <turan.shrinov@gmail.com>
2023-10-21 03:25:37 -0400

    updated

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -1,7 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+#include "main.h"
 
-void print_alphabet_x10(void);
-
-#endif
+/**
+ * main - Entry point
+ *
+ * Description: Calls the print_alphabet_x10 function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
 
